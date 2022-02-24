420cff5aa5dea4e2a51508fb6abf23f027ba9d5a
Damira <damira.cyprus@gmail.com>
2022-02-24 12:54:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 > 0) return false;
+  let strLength = str.length;
+  let bracketsLength = bracketsConfig.length;
+
+  for (let i = 0; i < strLength / 2; i++) {
+    for (let j = 0; j < bracketsLength; j++) {
+      str = str.replace(bracketsConfig[j].join(''), '');
+    }
+  }
+  return str.length > 0 ? false : true;
+};
